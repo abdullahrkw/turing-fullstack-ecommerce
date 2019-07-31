300ced97f85af588b9d89a6375725085e1be30e8
abdullahrkw <abdullah.nawazrkw@gmail.com>
2019-08-01 01:26:35 +0500

    API endpoint for price-read

---
+++ functions/test.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+exports.handler = (event, context, callback) => {
+    callback(null, {
+      statusCode: 200,
+      body: 'No worries  all is working fine!'
+    })
+  }  
---
+++ lambda/price-read.js
@@ -0,0 +1,28 @@
+const faunadb = require('faunadb');
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SERVER_SECRET
+});
+
+
+exports.handler = (event, context, callback) => {
+  console.log('Function `price-read` invoked');
+  const data = JSON.parse(event.body);
+  return client.query(q.Paginate(q.Match(q.Index("price-by-sku"),data.sku)))
+    .then((response) => {
+      const todoRefs = response.data[0][1];
+        return callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({price:todoRefs})
+        })
+     
+    })
+    .catch((error) => {
+      console.log('error', error)
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
+    })
+}
