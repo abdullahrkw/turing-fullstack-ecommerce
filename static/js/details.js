38ddb1543fd001f93017116414f1684adfaf04dd
abdullahrkw <abdullah.nawazrkw@gmail.com>
2019-08-03 22:33:25 +0500

    updating function

--- static/js/details.js
+++ static/js/details.js
@@ -15,7 +15,7 @@ let prStck =  new Vue({
         .then(response => {console.log('success', response);
                           
                             console.log(response.data);
-                            console.log(date[0][1]);
+                            console.log(response.date[0][1]);
                         })
         .catch(error => console.log('this is error',error));
       },
