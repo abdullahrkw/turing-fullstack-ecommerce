287c1695a1b16bf5e8f4ae7de4146aad381eb00e
abdullahrkw <abdullah.nawazrkw@gmail.com>
2019-08-03 22:06:07 +0500

    updating function

--- static/js/details.js
+++ static/js/details.js
@@ -12,7 +12,11 @@ let prStck =  new Vue({
     mounted: async function(){
         axios
         .post('https://xenodochial-mirzakhani-152330.netlify.com/.netlify/functions/price-read',JSON.stringify({'sku':'A13'}))
-        .then(response => console.log('succes', response))
+        .then(response => {console.log('success', response);
+                            let data = JSON.parse(response);
+                            console.log(data);
+                            console.log(date[0][1]);
+                        })
         .catch(error => console.log('this is error',error));
       },
     computed: {
